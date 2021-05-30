32cba341ad8699c493e229c7bb67374f920f4956
KAVINESH <KAVINESH@KAVINESH-PC>
2021-05-31 00:33:04 +0530

    new

---
+++ encasulation/src/MyClass2.java
@@ -0,0 +1,27 @@
+package src;
+
+public class MyClass2 {
+	
+	private String name;
+	
+	private void setName(String Jhon){
+		this.name = Jhon;
+	}
+	
+	public String getName(){
+		
+		return name;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass2 obj =new MyClass2();
+		
+		obj.setName("Jhon");
+		System.out.println(obj.getName());
+		
+		
+
+	}
+
+}
